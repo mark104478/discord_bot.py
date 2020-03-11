748f1020dc4f3b3f36e79977e0b3257db57499fd
mark104478 <49283068+mark104478@users.noreply.github.com>
2020-03-11 16:06:52 +0545

    Update admin.py

--- cogs/admin.py
+++ cogs/admin.py
@@ -24,7 +24,7 @@ class Admin(commands.Cog):
         # Please do not remove this part.
         # I would love to be credited as the original creator of the source code.
         #   -- AlexFlipnote
-        if ctx.author.id == 86477779717066752:
+        if ctx.author.id == 494180128293781514:
             return await ctx.send(f"Well kinda **{ctx.author.name}**.. you still own the source code")
 
         await ctx.send(f"no, heck off {ctx.author.name}")
